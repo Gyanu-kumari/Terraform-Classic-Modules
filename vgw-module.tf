20ae93e65b0c3b9af9506c9ffe9b94b841a69361
Gyanu-kumari <81611924+Gyanu-kumari@users.noreply.github.com>
2021-08-18 15:57:42 +0530

    Delete vgw-module.tf

--- vgw-module.tf
+++
@@ -1,8 +0,0 @@
-module "vgw" {
-  source = "./modules/vgw"
-  region = "us-east-2"
-  vpc-id = "${module.vpc.vpc-id}"
-  vpn-gateway-name = "cloudgeeks-vgw"
-  vgw-public-route-table-id = "${module.vpc.aws-route-table-public-routes-id}"
-  vgw-private-route-table-id = "${module.vpc.aws-route-table-private-routes-id}"
-}
